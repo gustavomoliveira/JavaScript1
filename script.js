c10cd8709c3bbb4fb427fa106d29e350595bfaa7
Gustavo Mendes <gustavo.moliveira@al.infnet.edu.br>
2024-04-17 22:13:46 +0200

    Definicao do calculo automatico da idade

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+//input
 anoNascimento = 1987
-idade = 2024 - anoNascimento
 nome = "Gustavo"
+
+//processamento
+hoje = new Date()
+anoAtual = hoje.getFullYear()
+idade = anoAtual - anoNascimento
 mensagem = "Sou" + nome + "e tenho" + idade + "anos"
 
+//output
 alert(mensagem)
